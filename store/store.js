84b409c46fc75ac9f5e2e3b6211be6f0b34f1d84
Lioz Fired <friedlioz@gmail.com>
2025-08-15 09:04:21 +0300

    Create store.js and appReducer func

---
+++ store/store.js
@@ -0,0 +1,17 @@
+const { createStore } = Redux
+
+export const SET_TODOS = 'SET_TODOS'
+
+const initialState = {
+    todos: [],
+    // isLoading: false,
+    // currentFilterBy: null,
+    // loggedinUser: null,
+}
+
+export function appReducer(state = initialState, cmd = {}) {
+    switch (cmd.type) {
+        case SET_TODOS:
+            return { ...state, todos: cmd.todos }
+    }
+}
