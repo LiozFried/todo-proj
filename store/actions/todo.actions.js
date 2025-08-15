d63535e2d2d2874f4247d93ff9c2fc7e0b34390b
Lioz Fired <friedlioz@gmail.com>
2025-08-15 09:51:54 +0300

    Support filtering in loadTodos func

--- store/actions/todo.actions.js
+++ store/actions/todo.actions.js
@@ -1,7 +1,7 @@
 import { todoService } from '../../services/todo.service.js'
 import { store, SET_TODOS } from '../store.js'
 
-export function loadTodos() {
-    return todoService.query()
+export function loadTodos(filterBy) {
+    return todoService.query(filterBy)
         .then(todos => store.dispatch({ type: SET_TODOS, todos }))
 }
