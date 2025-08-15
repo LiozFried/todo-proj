4dfe61795913c5bc3b5466484c45f0c016a09af5
Lioz Fired <friedlioz@gmail.com>
2025-08-15 10:24:52 +0300

    Add setFIlterBy func

--- store/actions/todo.actions.js
+++ store/actions/todo.actions.js
@@ -1,7 +1,12 @@
 import { todoService } from '../../services/todo.service.js'
-import { store, SET_TODOS } from '../store.js'
+import { store, SET_TODOS, SET_FILTER } from '../store.js'
 
 export function loadTodos(filterBy) {
     return todoService.query(filterBy)
         .then(todos => store.dispatch({ type: SET_TODOS, todos }))
+}
+
+export function setFilterBy(searchParams) {
+    return todoService.getFilterFromSearchParams(searchParams)
+        .then(filterBy => store.dispatch({ type: SET_FILTER, filterBy }))
 }
