dc6cc56a7a3b2c97ecc947d30689a2c09ee7b02d
Lioz Fired <friedlioz@gmail.com>
2025-08-16 21:34:25 +0300

    Update the filter state

--- store/actions/todo.actions.js
+++ store/actions/todo.actions.js
@@ -1,12 +1,12 @@
 import { todoService } from '../../services/todo.service.js'
 import { store, SET_TODOS, SET_FILTER } from '../store.js'
 
-export function loadTodos(filterBy) {
+export function loadTodos() {
+    const { filterBy } = store.getState()
     return todoService.query(filterBy)
         .then(todos => store.dispatch({ type: SET_TODOS, todos }))
 }
 
-export function setFilterBy(searchParams) {
-    return todoService.getFilterFromSearchParams(searchParams)
-        .then(filterBy => store.dispatch({ type: SET_FILTER, filterBy }))
+export function setFilterBy(filterBy) {
+    store.dispatch({ type: SET_FILTER, filterBy })
 }
