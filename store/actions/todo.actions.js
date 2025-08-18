8ed481d01d4cadc62182fe562eace39d526352bf
Lioz Fired <friedlioz@gmail.com>
2025-08-18 18:34:30 +0300

    Change loadTodos to supporting max page and done percent

--- store/actions/todo.actions.js
+++ store/actions/todo.actions.js
@@ -1,12 +1,29 @@
 import { todoService } from '../../services/todo.service.js'
-import { store, SET_TODOS, SET_FILTER } from '../store.js'
+import { store, SET_TODOS, SET_FILTER, SET_IS_LOADING, SET_DONE_TODOS_PERCENT, SET_MAX_PAGE } from '../store.js'
 
 export function loadTodos() {
+    store.dispatch({ type: SET_IS_LOADING, isLoading: true })
     const { filterBy } = store.getState()
     return todoService.query(filterBy)
-        .then(todos => store.dispatch({ type: SET_TODOS, todos }))
+        .then(({ todos, maxPage, doneTodosPercent }) => {
+            store.dispatch({ type: SET_TODOS, todos })
+            _setTodosData(doneTodosPercent, maxPage)
+            return todos
+        })
+        .catch((err) => {
+            console.error('Cannot load todos:', err)
+            throw err
+        })
+        .finally(() => {
+            store.dispatch({ type: SET_IS_LOADING, isLoading: false })
+        })
 }
 
 export function setFilterBy(filterBy) {
     return { type: SET_FILTER, filterBy }
+}
+
+function _setTodosData(doneTodosPercent, maxPage) {
+    store.dispatch({ type: SET_DONE_TODOS_PERCENT, doneTodosPercent })
+    store.dispatch({ type: SET_MAX_PAGE, maxPage })
 }
