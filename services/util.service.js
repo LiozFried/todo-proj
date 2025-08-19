d351e967ec541278911555ab96265eaae84bb1a9
Lioz Fired <friedlioz@gmail.com>
2025-08-19 18:28:12 +0300

    Import some funcs from the class

--- services/util.service.js
+++ services/util.service.js
@@ -4,7 +4,11 @@ export const utilService = {
     getRandomIntInclusive,
     loadFromStorage,
     saveToStorage,
-    animateCSS
+    animateCSS,
+    getRandomColor,
+    debounce,
+    getFormattedTime,
+    getTruthyValues,
 }
 
 function makeId(length = 6) {
@@ -19,12 +23,12 @@ function makeId(length = 6) {
 }
 
 function makeLorem(size = 100) {
-    const words = ['The sky', 'above', 'the port', 'was', 'the color' ,'of nature', 'tuned', 'to', 'a live channel', 'All', 'this happened', 'more or less', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', 'a pleasure', 'to', 'burn']
+    const words = ['The sky', 'above', 'the port', 'was', 'the color', 'of nature', 'tuned', 'to', 'a live channel', 'All', 'this happened', 'more or less', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', 'a pleasure', 'to', 'burn']
     var txt = ''
     while (size > 0) {
         size--
         txt += words[Math.floor(Math.random() * words.length)]
-        if (size >= 1 ) txt += ' '
+        if (size >= 1) txt += ' '
     }
     return txt
 }
@@ -44,7 +48,7 @@ function loadFromStorage(key) {
     return (data) ? JSON.parse(data) : undefined
 }
 
-function animateCSS(el, animation='bounce') {
+function animateCSS(el, animation = 'bounce') {
     const prefix = 'animate__'
     return new Promise((resolve, reject) => {
         const animationName = `${prefix}${animation}`
@@ -57,4 +61,58 @@ function animateCSS(el, animation='bounce') {
 
         el.addEventListener('animationend', handleAnimationEnd, { once: true })
     })
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function debounce(func, wait) {
+    let timeout;
+
+    return function executedFunction(...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            func(...args)
+        }, wait);
+    };
+};
+
+
+function getFormattedTime(at) {
+    const timeDiff = Date.now() - at
+    const minutes = +(timeDiff / (1000 * 60)).toFixed(0)
+    if (minutes < 60) return minutes + ' minutes ago'
+    else if (minutes < 60 * 2) return 'About an hour ago'
+    else if (minutes < 60 * 24) return 'Several hours ago'
+    return 'A day or more ago'
+}
+
+
+function getTruthyValues(obj) {
+    const newObj = {}
+    for (const key in obj) {
+        const val = obj[key]
+        if (val) {
+            newObj[key] = val
+        }
+    }
+    return newObj
+}
+
+
+export function setCssVarVal(cssVar, value, selector = ':root') {
+    const el = document.querySelector(selector)
+    el.style.setProperty(cssVar, value)
+}
+
+export function getCssVarVal(cssVar, selector = ':root') {
+    const el = document.querySelector(selector)
+    const rootStyle = getComputedStyle(el)
+    return rootStyle.getPropertyValue(cssVar)
 }
