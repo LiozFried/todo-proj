1fbb60d082f16774253dfd5e69a0bb830c9302d3
Lioz Fired <friedlioz@gmail.com>
2025-08-19 17:55:34 +0300

    Add more funcs to todo action

--- store/actions/todo.actions.js
+++ store/actions/todo.actions.js
@@ -1,5 +1,6 @@
 import { todoService } from '../../services/todo.service.js'
 import { store, SET_TODOS, SET_FILTER, SET_IS_LOADING, SET_DONE_TODOS_PERCENT, SET_MAX_PAGE, ADD_TODO, UPDATE_TODO, REMOVE_TODO } from '../store.js'
+import { addActivity } from './user.action.js'
 
 export function loadTodos() {
     store.dispatch({ type: SET_IS_LOADING, isLoading: true })
@@ -16,6 +17,38 @@ export function loadTodos() {
         })
         .finally(() => {
             store.dispatch({ type: SET_IS_LOADING, isLoading: false })
+        })
+}
+
+export function saveTodo(todo) {
+    const type = todo._id ? UPDATE_TODO : ADD_TODO
+    return todoService.save(todo)
+        .then(({ maxPage, doneTodosPercent, savedTodo }) => {
+            store.dispatch({ type, todo: savedTodo })
+            _setTodosData(doneTodosPercent, maxPage)
+            return savedTodo
+        })
+        .then((res) => {
+            const actioName = todo._id ? 'Update' : 'Added'
+            return addActivity(`${actioName} a Todo: ${todo.txt}`)
+                .then(() => res)
+        })
+        .catch((err) => {
+            console.error('Cannot save todo:', err)
+            throw err
+        })
+}
+
+export function removeTodo(todoId) {
+    return todoService.remove(todoId)
+        .then(({ maxPage, doneTodosPercent }) => {
+            store.dispatch({ type: REMOVE_TODO, todoId })
+            _setTodosData(doneTodosPercent, maxPage)
+        })
+        .then(() => addActivity('Removed the todo: ' + todoId))
+        .catch((err) => {
+            console.error('Cannot remove todo:', err)
+            throw err
         })
 }
 
