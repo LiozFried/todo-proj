7ac66937cc40596eac3c14505af60a6b9715367e
Lioz Fired <friedlioz@gmail.com>
2025-08-19 17:20:27 +0300

    Impove user service

--- services/user.service.js
+++ services/user.service.js
@@ -1,6 +1,5 @@
 import { storageService } from "./async-storage.service.js"
 
-
 export const userService = {
     getLoggedinUser,
     login,
@@ -8,8 +7,13 @@ export const userService = {
     signup,
     getById,
     query,
-    getEmptyCredentials
+    getEmptyCredentials,
+    updateBalance,
+    updateUserPrefs,
+    getDefaultUserPref,
+    addActivity,
 }
+
 const STORAGE_KEY_LOGGEDIN = 'user'
 const STORAGE_KEY = 'userDB'
 
@@ -31,7 +35,15 @@ function login({ username, password }) {
 }
 
 function signup({ username, password, fullname }) {
-    const user = { username, password, fullname }
+    const user = {
+        username,
+        password,
+        fullname,
+        balance: 10000,
+        activities: [],
+        pref: getDefaultUserPref()
+    }
+
     user.createdAt = user.updatedAt = Date.now()
 
     return storageService.post(STORAGE_KEY, user)
@@ -47,8 +59,75 @@ function getLoggedinUser() {
     return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN))
 }
 
+function updateBalance(diff) {
+    const loggedinUser = getLoggedinUser()
+    if (!loggedinUser) return
+
+    return getById(loggedinUser._id)
+        .then(user => {
+            user.balance += diff
+
+            return storageService.put(STORAGE_KEY, user)
+                .then((user) => {
+                    _setLoggedinUser(user)
+                    return user.balance
+                })
+        })
+}
+
+function updateUserPrefs(userToUpdate) {
+    const loggedinUser = getLoggedinUser()
+    if (!loggedinUser) return
+
+    return getById(loggedinUser._id)
+        .then(user => {
+            user = { ...user, ...userToUpdate }
+
+            return storageService.put(STORAGE_KEY, user)
+                .then((savedUser) => {
+                    _setLoggedinUser(savedUser)
+                    return savedUser
+                })
+        })
+}
+
+function addActivity(txt) {
+    const activity = {
+        txt,
+        at: Date.now()
+    }
+
+    const loggedinUser = getLoggedinUser()
+    if (!loggedinUser) return Promise.reject('No loggedin user')
+
+    return getByIdgetById(loggedinUser._id)
+        .then(user => {
+            if (!user.activities) user.activities = []
+            user.activities.unshift(activity)
+            return user
+        })
+        .then(userToUpdate => {
+            return storageService.put(STORAGE_KEY, userToUpdate)
+                .then((savedUser) => {
+                    _setLoggedinUser(savedUser)
+                    return savedUser
+                })
+        })
+}
+
+function getDefaultUserPref() {
+    return { color: '#eeeeee', bgc: '#000000', fullname: '' }
+}
+
 function _setLoggedinUser(user) {
-    const userToSave = { _id: user._id, fullname: user.fullname }
+    const userToSave = {
+        _id: user._id,
+        fullname: user.fullname,
+        balance: user.balance,
+        pref: user.pref,
+        activities: user.activities
+    }
+
     sessionStorage.setItem(STORAGE_KEY_LOGGEDIN, JSON.stringify(userToSave))
     return userToSave
 }
@@ -56,8 +135,8 @@ function _setLoggedinUser(user) {
 function getEmptyCredentials() {
     return {
         fullname: '',
-        username: 'muki',
-        password: 'muki1',
+        username: '',
+        password: '',
     }
 }
 
